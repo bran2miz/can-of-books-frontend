a65bedc8b58bd27aee20fa670acacb5a6cecddb2
brandonmizutani <bran2miz@gmail.com>
2021-10-28 14:01:27 -0700

    add the BookFormModal

--- src/Components/BookFormModal.js
+++ src/Components/BookFormModal.js
@@ -7,31 +7,32 @@ class BookFormModal extends React.Component{
     render(){
         return(
             <>
-            <Form>
+            <Form onSubmit={this.handleBookSubmit}>
                 <Form.Group className = "mb-3" controlId = "bookTitle">
                     <Form.Label>
+                      Title
                     </Form.Label>
-                        <Form.Control />
-
+                        <Form.Control type="title" placeholder="Enter Your Book Here!" />
                 </Form.Group>
-                <Form.Group>
+                <Form.Group className="mb-3" controlID="bookDescription">
                     <Form.Label>
+                      Description of Book
                     </Form.Label>
-                        <Form.Control />
-
+                        <Form.Control type="description" placeholder="Enter Your Book Description"/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group className="mb-3" controlID="userEmail">
                     <Form.Label>
+                      Email
                     </Form.Label>
-                        <Form.Control />
-
+                        <Form.Control type="email" placeholder="Enter Your Email"/>
                 </Form.Group>
-                <Form.Group>
-                    <Form.Label>
-                    </Form.Label>
-                        <Form.Control />
-
+                <Form.Group className="mb-3" controlId="statusCheckbox">
+                  <Form.Check type="checkbox" label="Read" />
                 </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  SUBMIT BOOK
+                </Button>
             </Form>
 
             </>
@@ -39,3 +40,5 @@ class BookFormModal extends React.Component{
     }
 
 }
+
+export default BookFormModal;
