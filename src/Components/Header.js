f217bafa55046dac81f3bc602bcc0e14dcd10798
Ian Cargill <idcargill@gmail.com>
2021-11-04 16:34:53 -0700

    REFACTOR components

---
+++ src/Components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Navbar, NavItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../CSS/Header.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+          <Navbar.Brand>My Favorite Books</Navbar.Brand>
+
+          <NavItem>
+            <Link to='/' className='nav-link'>
+              Home
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to='/profile' className='nav-link'>
+              Profile
+            </Link>
+          </NavItem>
+        </Navbar>
+      </>
+    );
+  }
+}
+
+export default Header;
